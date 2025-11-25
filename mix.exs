380a8a1803c0937f65446f0786b9eb70e31ce1a8
Lars Wikman <lars@underjord.io>
2025-11-25 07:58:34 +0100

    Version 0.2.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Confuse.MixProject do
   def project() do
     [
       app: :confuse,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.1",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
